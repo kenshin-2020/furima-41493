45ab035ba0f31ff953a0d917c6f79b8029eb8841
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-09 06:58:42 +0900

    単体テストの新規登録ができる場合の設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
       it 'nicknameとemail、passwordとpassword_confirmation、last_nameとfirst_name、last_name_readingとfirst_name_reading、birth_dateが存在すれば登録できる' do
+        expect(@user).to be_valid
       end
     end
     context '新規登録できない場合' do
