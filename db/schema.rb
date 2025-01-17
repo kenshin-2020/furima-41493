d489fc38ab47daedc40f8cb690e77677ef54d251
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-18 06:45:46 +0900

    商品出品のためのモデルとテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_25_001615) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_17_213344) do
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "item_text", null: false
+    t.integer "category_id", null: false
+    t.integer "situation_id", null: false
+    t.integer "shippingfee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "deliveryday_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_25_001615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
