f5148ec84717ec4d2c78c8b6e2cb8e0ce66edf42
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-18 17:13:34 +0900

    itemモデルのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
-  validates :item_name, presence: true
-  validates :item_text, presence: true
-  validates :price, presence: true
-
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shippingfee_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :deliveryday_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_name,      presence: true
+  validates :item_text,      presence: true
+  validates :category_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :situation_id,   presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shippingfee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,  presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :deliveryday_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,          presence: true,
+                             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'must be a number between ¥300 and ¥9,999,999' }
 
   has_one_attached :image
 end
