2e3163de70a1acbdd7ceb8324f5d75ee8773d472
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-18 16:25:18 +0900

    ActiveHashのcategoryモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+
   has_one_attached :image
 end
