9ff935ce1e2ed885768e96798edca12688f57777
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-18 18:01:13 +0900

    imageカラムのバリデーション設定とエラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
+  validates :image,          presence: true
   validates :item_name,      presence: true
   validates :item_text,      presence: true
   validates :category_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
