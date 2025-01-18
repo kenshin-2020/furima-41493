e95e3ac57f16e1cd984ed3887f1f63014007711b
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-18 15:38:47 +0900

    itemsコントローラーのcreateアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_text, :category_id, :situation_id, :shippingfee_id, :prefecture_id,
+                                 :deliveryday_id, :price, :image).merge(user_id: current_user.id)
   end
 end
