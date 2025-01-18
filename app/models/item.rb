add8ad098744e51c68faeb4deefc715e3119284d
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-18 17:04:07 +0900

    ActiveHashのdeliveryモデル、prefectureモデル、shippingfeeモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,13 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :item_text, presence: true
+  validates :price, presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shippingfee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :deliveryday_id, numericality: { other_than: 1, message: "can't be blank" }
 
   has_one_attached :image
 end
