ac26c625e8786f69d24e2c33e0cdb5f23310d04c
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-19 11:12:42 +0900

    商品出品のテストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,5 @@ class Item < ApplicationRecord
                              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'must be a number between ¥300 and ¥9,999,999' }
 
   has_one_attached :image
+  belongs_to :user
 end
