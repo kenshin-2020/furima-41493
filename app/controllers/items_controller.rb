b649d487d39ffc79e33107b50b1301ca47b80aea
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-01-30 07:07:59 +0900

    updateアクションの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,9 +29,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
     else
       render :edit, status: :unprocessable_entity
     end
