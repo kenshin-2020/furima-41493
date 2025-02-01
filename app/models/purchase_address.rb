fbf990dd5af78f84ff3f98693d0e0a42178b6f40
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-02-02 08:02:50 +0900

    テーブルにデータを保存する処理を記載

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -12,4 +12,10 @@ class PurchaseAddress
                              length: { in: 10..11, message: 'must be between 10 and 11 digits' }
   end
   validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture.id, city: city, house_number: house_number,
+                   building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
 end
