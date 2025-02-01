67f1a9adb5fedd1ae1a206a9811c1a1212208708
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-02-01 19:56:52 +0900

    Formオブジェクトのバリデーションを設定

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,15 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d+\z/, message: 'must contain only numbers' },
+                             length: { in: 10..11, message: 'must be between 10 and 11 digits' }
+  end
+  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+end
