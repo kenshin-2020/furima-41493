b73d0021b31c86688b9e17c4e104ad47a5c9ef45
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-02-09 08:39:58 +0900

    purchase_addressのFactoryBotの作成

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :purchase_address do
+    postal_code { '123-4567' }
+    prefecture_id { 5 }
+    city { '東京都' }
+    house_number { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_number { '08088888888' }
+    association :user
+    association :item
+  end
+end
