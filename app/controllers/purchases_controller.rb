1837c823b37727c70dc4ffc0b13799d78473dd7e
kenshin-2020 <koji.matsumoto110@gmail.com>
2025-02-09 08:26:13 +0900

    エラーメッセージの表示を実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
   end
 
   def new
@@ -9,21 +10,20 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
-      logger.debug '✅ PurchaseAddress is valid!'
       @purchase_address.save
       redirect_to root_path
     else
-      logger.debug "❌ PurchaseAddress is invalid: #{@purchase_address.errors.full_messages}"
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
 
   def purchase_params
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name,
-                  :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name,
+                                             :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
